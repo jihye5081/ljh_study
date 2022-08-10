a4d58033356e6abdfb492babf1ff6484ce8e364b
gpffh0001 <ekftn90@gmail.com>
2022-08-10 23:17:26 +0900

    Add: useEffect 공부

--- create-react/src/App.js
+++ create-react/src/App.js
@@ -1,11 +1,86 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+// import { useState, useEffect } from "react";
+
+// function App() {
+//   const [counter, setValue] = useState(0);
+//   const [keyword, setKeyword] = useState("");
+//   const onClick = () => setValue((prev) => prev + 1);
+//   const onChange = (event) => setKeyword(event.target.value);
+//   // useEffect => 2개의 인자를 갖는 함수, 리액트 컴포넌트가 렌더링 될 때마다 특정 작업을 실행할 수 있도록 하는 hook
+//   // 첫 번째 인자는 실행시키고 싶은 코드
+//   // 두 번째 인자는 dependencies(== react.js가 지켜보아야하는 것들) -> 지켜보고 변화할 때 react.js가 코드를 실행
+//   // const iRunOnlyOnce = () => {
+//   //   console.log("i run only once");
+//   // };
+//   useEffect(() => {
+//     // 두 번째 인자가 비어있으므로(react.js가 지켜볼 대상이 없음을 의미) 딱 한 번만 실행
+//     console.log("i run only once");
+//   }, []);
+//   // 시작할 때, keyword가 변화할 때 한 번 실행
+//   // [keyword] => keyword가 변화할 때마다 실행됨
+//   useEffect(() => {
+//     console.log("i run when 'keyword' changes.");
+//   }, [keyword]);
+//   useEffect(() => {
+//     console.log("i run when 'counter' changes.");
+//   }, [counter]);
+//   useEffect(() => {
+//     console.log("i run when keyword & counter changes.");
+//   }, [keyword, counter]);
+//   return (
+//     <div>
+//       <input
+//         value={keyword}
+//         onChange={onChange}
+//         type="text"
+//         placeholder="Search here..."
+//       />
+//       <h1>{counter}</h1>
+//       <button onClick={onClick}>click me</button>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import { useState, useEffect } from "react";
+
+function Hello() {
+  // 밑의 코드와 같은 것
+  // function byFn() {
+  //   console.log("bye :(");
+  // }
+  // function hiFn() {
+  //   console.log("created :)");
+  //   return byFn;
+  // }
+  // useEffect(hiFn, []);
+  useEffect(() => {
+    console.log("created :)");
+    // clean up 함수
+    return () => console.log("destroyed :(");
+  }, []);
+  useEffect(() => {
+    console.log("hi :)");
+    return () => console.log("bye :(");
+  }, []);
+  // 위의 코드와 같은 것
+  // useEffect(function () {
+  //   console.log("hi :)");
+  //   return function () {
+  //     console.log("bye :(");
+  //   };
+  // }, []);
+
+  return <h1>Hello</h1>;
+}
 
 function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Continue"} />
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
--- create-react/src/index.js
+++ create-react/src/index.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // strictmode -> 개발 과정 중 안전을 위해 켜는 기능으로 배포시 자동으로 해제됨
+  // <StrictMode>
+  <App />,
+  // </StrictMode>,
   document.getElementById("root")
 );
