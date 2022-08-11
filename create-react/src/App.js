a41ebced5aaa21fde9b1dfac235f633c34329ae9
gpffh0001 <ekftn90@gmail.com>
2022-08-11 22:48:25 +0900

    Add: To Do List 만들기

--- create-react/src/App.js
+++ create-react/src/App.js
@@ -1,86 +1,40 @@
-// import { useState, useEffect } from "react";
-
-// function App() {
-//   const [counter, setValue] = useState(0);
-//   const [keyword, setKeyword] = useState("");
-//   const onClick = () => setValue((prev) => prev + 1);
-//   const onChange = (event) => setKeyword(event.target.value);
-//   // useEffect => 2개의 인자를 갖는 함수, 리액트 컴포넌트가 렌더링 될 때마다 특정 작업을 실행할 수 있도록 하는 hook
-//   // 첫 번째 인자는 실행시키고 싶은 코드
-//   // 두 번째 인자는 dependencies(== react.js가 지켜보아야하는 것들) -> 지켜보고 변화할 때 react.js가 코드를 실행
-//   // const iRunOnlyOnce = () => {
-//   //   console.log("i run only once");
-//   // };
-//   useEffect(() => {
-//     // 두 번째 인자가 비어있으므로(react.js가 지켜볼 대상이 없음을 의미) 딱 한 번만 실행
-//     console.log("i run only once");
-//   }, []);
-//   // 시작할 때, keyword가 변화할 때 한 번 실행
-//   // [keyword] => keyword가 변화할 때마다 실행됨
-//   useEffect(() => {
-//     console.log("i run when 'keyword' changes.");
-//   }, [keyword]);
-//   useEffect(() => {
-//     console.log("i run when 'counter' changes.");
-//   }, [counter]);
-//   useEffect(() => {
-//     console.log("i run when keyword & counter changes.");
-//   }, [keyword, counter]);
-//   return (
-//     <div>
-//       <input
-//         value={keyword}
-//         onChange={onChange}
-//         type="text"
-//         placeholder="Search here..."
-//       />
-//       <h1>{counter}</h1>
-//       <button onClick={onClick}>click me</button>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-import { useState, useEffect } from "react";
-
-function Hello() {
-  // 밑의 코드와 같은 것
-  // function byFn() {
-  //   console.log("bye :(");
-  // }
-  // function hiFn() {
-  //   console.log("created :)");
-  //   return byFn;
-  // }
-  // useEffect(hiFn, []);
-  useEffect(() => {
-    console.log("created :)");
-    // clean up 함수
-    return () => console.log("destroyed :(");
-  }, []);
-  useEffect(() => {
-    console.log("hi :)");
-    return () => console.log("bye :(");
-  }, []);
-  // 위의 코드와 같은 것
-  // useEffect(function () {
-  //   console.log("hi :)");
-  //   return function () {
-  //     console.log("bye :(");
-  //   };
-  // }, []);
-
-  return <h1>Hello</h1>;
-}
+import { useState } from "react";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    // 함수를 통해 수정하는 방법
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    // 직접적으로 값을 수정하는 방법
+    setToDo("");
+  };
+  console.log(toDos);
+  console.log(toDos.map((item, index) => <li key={index}>{item}</li>));
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do"
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      {/*map은 하나의 array에 있는 item을 내가 원하는 무엇이든지로 바꿔주는 역할, 바꿘 것을 새로운 배열로 return*/}
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
