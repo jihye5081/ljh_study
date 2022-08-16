273ebbab28a3dc8aeecb2bf372080fb3949397de
gpffh0001 <ekftn90@gmail.com>
2022-08-16 17:25:40 +0900

    Add: Movie App part one

--- create-react/src/App.js
+++ create-react/src/App.js
@@ -1,34 +1,22 @@
-import { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  // 만약 default값이 비어있다면(array가 아니라면) undefined이므로 []를 통해 default값 부여
-  const [coins, setCoins] = useState([]);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const response = await fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=yaer`
+    );
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  // 빈 배열 == 어떤 것도 주시하거나 의존하지 않고 있다는 것 의미
+  //          이 코드가 한 번만 실행됨을 의미
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
+    getMovies();
   }, []);
-  return (
-    <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
-      {loading ? (
-        <strong>Loading...</strong>
-      ) : (
-        <select>
-          {coins.map((coin) => (
-            // warning 제거를 위해 key={coin.id} 작성
-            <option key={coin.id}>
-              {coins.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
-            </option>
-          ))}
-        </select>
-      )}
-    </div>
-  );
+  return <div>{loading ? <h1>Loading...</h1> : null}</div>;
 }
 
 export default App;
