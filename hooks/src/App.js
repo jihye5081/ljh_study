c9f31eb538aaa47a72bc12b1caff653a08766731
gpffh0001 <ekftn90@gmail.com>
2022-09-03 20:50:21 +0900

    Add: useTabs 예제 연습

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,37 +1,40 @@
-import { type } from "@testing-library/user-event/dist/type";
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-const useInput = (initualValue, validator) => {
-  const [value, setValue] = useState(initualValue);
-  const onChange = (event) => {
-    const {
-      target: { value },
-    } = event;
-    let willUpdate = true;
-    // validator가 function이 아닌 다른 타입이 들어오면 validator(value)에서 에러가 발생하므로 함수인지 타입 확인
-    if (typeof validator === "function") {
-      willUpdate = validator(value);
-    }
-    if (willUpdate) {
-      setValue(value);
-    }
+const content = [
+  {
+    tab: "Section 1",
+    content: "I'm the content of the Section 1",
+  },
+  {
+    tab: "Section 2",
+    content: "I'm the content of the Section 2",
+  },
+];
+
+// useTab => 버튼을 클릭했을 때 해당 버튼의 내용을 다르게 보여주는 것
+const useTabs = (initalTab, allTabs) => {
+  const [currentIndex, setCurrentIndex] = useState(initalTab);
+  if (!allTabs || !Array.isArray(allTabs)) {
+    return;
+  }
+  // 최상위에서만 hook을 호출해야하는데 이 위치에 존재하면 에러 발생
+  // 맨 위로 코드 옮겨주면 에러 해결
+  // const [currentIndex, setCurrentIndex] = useState(initalTab);
+  return {
+    currentItem: allTabs[currentIndex],
+    changeItem: setCurrentIndex,
   };
-  return { value, onChange };
 };
 
 const App = () => {
-  const maxLen = (value) => value.length <= 10;
-  // @가 포함되어 있으면 동작하지 x
-  // const maxLen = (value) => !value.includes("@");
-  const name = useInput("Mr.", maxLen);
+  const { currentItem, changeItem } = useTabs(0, content);
   return (
     <div>
-      <h1>Hello</h1>
-      <input placeholder="Name" {...name} />
-      {/* value={name.value} == {...name}
-          ...name => name 안에 있는 모든 것들을 풀어줌
-      */}
+      {content.map((section, index) => (
+        <button onClick={() => changeItem(index)}>{section.tab}</button>
+      ))}
+      <div>{currentItem.content}</div>
     </div>
   );
 };
